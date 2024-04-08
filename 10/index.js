c6e108d9a1d83a7d52cf646c5e31172965d012c7
Alisa <125174970+Alixann@users.noreply.github.com>
2024-04-08 12:51:59 +0900

    index.js

--- 10/index.js
+++ 10/index.js
@@ -1,6 +1,5 @@
 //1
 
-// // Создаем объект student
 // const student = {
 //     stack: ['HTML'], // значение по умолчанию
 //     level: 1, // значение по умолчанию
@@ -28,45 +27,7 @@
 //           }
 //       }
 
-//       return this; // возвращаем обновленный объект student
-//     }
-//   };
-
-//   // Пример использования метода improveLevel
-//   student.improveLevel().improveLevel().improveLevel().improveLevel().improveLevel();
-
-//   console.log(student.stack); // Результат: ["HTML", "CSS", "JavaScript", "React", "NodeJS"]
-
-//   //2
-//   // Создаем объект student
-// const student = {
-//     stack: ['HTML'], // значение по умолчанию
-//     level: 1, // значение по умолчанию
-//     improveLevel: function() {
-//       // Увеличиваем уровень
-//       this.level++;
-
-//       // Добавляем технологии в зависимости от уровня
-//       switch(this.level) {
-//         case 2:
-//           this.stack.push('CSS');
-//           break;
-//         case 3:
-//           this.stack.push('JavaScript');
-//           break;
-//         case 4:
-//           this.stack.push('React');
-//           break;
-//         case 5:
-//           this.stack.push('NodeJS');
-//           break;
-//         default:
-//           if (this.level > 5) {
-//             alert("Студент выучил все технологии!");
-//           }
-//       }
-
-//       return this; // возвращаем обновленный объект student
+//       return this; //
 //     }
 //   };
 
