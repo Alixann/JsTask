dfb1d8b549989e6b192f7107d25172d8a6b72283
Alisa <125174970+Alixann@users.noreply.github.com>
2024-04-09 11:06:45 +0900

    index.js

--- 10/index.js
+++ 10/index.js
@@ -36,6 +36,59 @@
 
 //   console.log(student.stack); // Результат: ["HTML", "CSS", "JavaScript", "React", "NodeJS"]
 
+//2 задание
+// const dog = {
+//   name: "Чарли",
+//   type: "Собака",
+//   makeSound() {
+//     return "гав-гав";
+//   },
+// };
+// const bird = {
+//   name: "Петя",
+//   type: "Воробей",
+//   makeSound() {
+//     return "чик-чирик";
+//   },
+// };
+
+//bind==============
+// function makeDomestic(isDomestic) {
+//   console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+//   return {
+//     ...this,
+//     isDomestic,
+//   };
+// }
+
+// const boundMakeDomestic = makeDomestic.bind(dog);
+// const domesticDog = boundMakeDomestic(true);
+// console.log(domesticDog);
+
+//call==============
+// function makeDomestic(isDomestic) {
+//   console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+//   return {
+//     ...this,
+//     isDomestic,
+//   };
+// }
+
+// const domesticBird = makeDomestic.call(bird, false);
+// console.log(domesticBird);
+
+//apply ==========
+// function makeDomestic(isDomestic) {
+//   console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+//   return {
+//     ...this,
+//     isDomestic,
+//   };
+// }
+
+// const domesticBird = makeDomestic.apply(bird, [false]);
+// console.log(domesticBird);
+
 //3
 const footballer = {
   fullName: "Cristiano Ronaldo",
